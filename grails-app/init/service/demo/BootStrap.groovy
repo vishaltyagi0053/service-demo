ce3c1b767c49e27507e7134308c75c04b27325e6
vishalkumar <vishal.kumar@cloudwords.com>
2018-03-29 19:34:56 +0530

    demo-1 commited

--- grails-app/init/service/demo/BootStrap.groovy
+++ grails-app/init/service/demo/BootStrap.groovy
@@ -2,8 +2,18 @@ package service.demo
 
 class BootStrap {
 
+    UserService userService
     def init = { servletContext ->
+        if (User.count() == 0) {
+            createUserData()
+        }
     }
     def destroy = {
+    }
+
+
+    void createUserData() {
+        userService.createUserData()
+        println "*******************Userdata created*******************"
     }
 }
